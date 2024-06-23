2ce570a2bf219274fe708dfff9c1b2d29278a54a
taitapcode <hoangductai2007@gmail.com>
2024-06-23 21:00:19 +0700

    add error label on create new project

--- src/components/CreateNewProject/CreateNewProject.tsx
+++ src/components/CreateNewProject/CreateNewProject.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import cn from 'utils/cn';
 import useCreateNewProjectState from 'state/createNewProject';
 import { CirclePlus, Plus } from 'lucide-react';
@@ -21,6 +22,12 @@ const CreateNewProject: React.FC = () => {
     if (!projectName) return setError('Project name cannot be empty');
   };
 
+  useEffect(() => {
+    if (isModalOpen) return;
+    setProjectName('');
+    clearError();
+  }, [isModalOpen]);
+
   return (
     <>
       <button className='btn btn-success' onClick={() => setModalOpen(true)}>
@@ -30,16 +37,21 @@ const CreateNewProject: React.FC = () => {
       <Modal open={isModalOpen} onOutSideClick={() => setModalOpen(false)}>
         <div className='mb-56 flex w-96 flex-col items-center gap-4 rounded-xl bg-base-300 p-4'>
           <p className='text-2xl font-bold'>New Project</p>
-          <input
-            type='text'
-            className={cn(
-              'input input-bordered w-full max-w-xs',
-              isError && 'placeholder:text-error',
-            )}
-            placeholder='Enter your project name'
-            value={projectName}
-            onChange={(e) => setProjectName(e.target.value)}
-          />
+          <label className='w-full'>
+            <div className='label'>
+              <div className='label-text text-error'>{errorMessage}</div>
+            </div>
+            <input
+              type='text'
+              className={cn(
+                'input input-bordered w-full max-w-xs',
+                isError && 'placeholder:text-error',
+              )}
+              placeholder='Enter your project name'
+              value={projectName}
+              onChange={(e) => setProjectName(e.target.value)}
+            />
+          </label>
           <button className='btn btn-success w-full' onClick={handleCreateNewProject}>
             <Plus />
             Create
